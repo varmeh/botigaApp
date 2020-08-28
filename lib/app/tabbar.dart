b3f5254a9013f850ed1028f6540cd692c9b8f4d5
Varun Mehta <varun.sagi20@gmail.com>
2020-08-28 16:38:42 +0530

    flavor banner added to tab screen

--- lib/app/tabbar.dart
+++ lib/app/tabbar.dart
@@ -4,6 +4,8 @@ import 'stores/storeListScreen.dart';
 import 'orders/orderListScreen.dart';
 import 'profile/profileListScreen.dart';
 
+import '../util/index.dart' show FlavorBanner;
+
 class Tabbar extends StatefulWidget {
   static String route = 'tabbar';
 
@@ -22,34 +24,36 @@ class _TabbarState extends State<Tabbar> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Botiga'),
-      ),
-      body: SafeArea(
-        child: _selectedTab.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.store),
-            title: const Text('Botiga'),
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.receipt),
-            title: const Text('Orders'),
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.person),
-            title: const Text('Profile'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
+    return FlavorBanner(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Botiga'),
+        ),
+        body: SafeArea(
+          child: _selectedTab.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.store),
+              title: const Text('Botiga'),
+            ),
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.receipt),
+              title: const Text('Orders'),
+            ),
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.person),
+              title: const Text('Profile'),
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
       ),
     );
   }
