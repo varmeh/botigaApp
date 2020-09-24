bfa472805a2b5a7fba3dac4320fd7db7f6e455ff
Varun Mehta <varun@botiga.app>
2020-09-24 20:19:46 +0530

    theme update

--- lib/theme/appTheme.dart
+++ lib/theme/appTheme.dart
@@ -7,7 +7,10 @@ final _textThemeUpdate = TextTheme(
 );
 
 final _selectedIconTheme = IconThemeData(size: 28);
-final _unselectedIconTheme = IconThemeData(size: 24);
+final _unselectedIconTheme = IconThemeData(
+  size: 24,
+  color: Color.fromARGB(63, 18, 23, 21),
+);
 
 class AppTheme {
   static final light = lightTheme.copyWith(
@@ -17,6 +20,7 @@ class AppTheme {
       margin: EdgeInsets.only(top: 10.0, left: 8.0, right: 8.0),
     ),
     bottomNavigationBarTheme: lightTheme.bottomNavigationBarTheme.copyWith(
+      backgroundColor: Colors.white,
       selectedIconTheme: _selectedIconTheme,
       unselectedIconTheme: _unselectedIconTheme,
     ),
