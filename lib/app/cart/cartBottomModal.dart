544a0cd0ad49638ad44c766a03a1f13d80a4a5cc
Varun Mehta <varun@botiga.app>
2020-10-07 00:01:38 +0530

    open container animation added to cart model

--- lib/app/cart/cartBottomModal.dart
+++ lib/app/cart/cartBottomModal.dart
@@ -21,76 +21,57 @@ class CartBottomModal extends StatelessWidget {
   }
 
   Widget _cart(BuildContext context, CartProvider provider) {
+    final textStyle =
+        AppTheme.textStyle.w600.size(15).colored(AppTheme.backgroundColor);
     return Material(
       elevation: 3.0,
-      color: Colors.transparent,
+      // color: Colors.transparent,
       shadowColor: Colors.transparent,
       child: Container(
         height: 56,
         width: MediaQuery.of(context).size.width - 40,
-        decoration: BoxDecoration(
-          color: AppTheme.primaryColor,
-          borderRadius: new BorderRadius.only(
-            topLeft: const Radius.circular(16.0),
-            topRight: const Radius.circular(16.0),
+        child: OpenContainer(
+          closedShape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: const Radius.circular(16.0),
+              topRight: const Radius.circular(16.0),
+            ),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    BotigaIcons.basket,
-                    color: AppTheme.backgroundColor,
-                  ),
-                  SizedBox(width: 16.0),
-                  Text(
-                    '${provider.numberOfItemsInCart} Item${provider.numberOfItemsInCart > 1 ? 's' : ''}',
-                    style: AppTheme.textStyle.w600
-                        .size(15)
-                        .colored(AppTheme.backgroundColor),
-                  ),
-                ],
+          closedColor: AppTheme.primaryColor,
+          transitionDuration: Constants.kContainerAnimationDuration,
+          closedBuilder: (context, openContainer) {
+            return GestureDetector(
+              onTap: openContainer,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          BotigaIcons.basket,
+                          color: AppTheme.backgroundColor,
+                        ),
+                        SizedBox(width: 16.0),
+                        Text(
+                          '${provider.numberOfItemsInCart} Item${provider.numberOfItemsInCart > 1 ? 's' : ''}',
+                          style: textStyle,
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Rs. ${provider.totalPrice}',
+                      style: textStyle,
+                    ),
+                  ],
+                ),
               ),
-              Text(
-                'Rs. ${provider.totalPrice}',
-                style: AppTheme.textStyle.w600
-                    .size(15)
-                    .colored(AppTheme.backgroundColor),
-              ),
-            ],
-          ),
+            );
+          },
+          openBuilder: (_, __) => CartConfirmationScreen(),
         ),
       ),
     );
   }
 }
-
-// OpenContainer(
-//   closedColor: AppTheme.primaryColor,
-//   transitionDuration: Constants.kContainerAnimationDuration,
-//   closedBuilder: (context, openContainer) {
-//     return GestureDetector(
-//       onTap: openContainer,
-//       child: Container(
-//         child: Center(
-//           child: Badge(
-//             animationDuration: Duration(milliseconds: 100),
-//             badgeColor: AppTheme.backgroundColor,
-//             badgeContent: Text('${provider.numberOfItemsInCart}'),
-//             animationType: BadgeAnimationType.scale,
-//             child: Icon(
-//               Icons.shopping_cart,
-//               color: AppTheme.backgroundColor,
-//               size: 35,
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   },
-//   openBuilder: (_, __) => CartConfirmationScreen(),
-// ),
