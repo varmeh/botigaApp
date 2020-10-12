410fad81b1a5ec37c8ce2ae8054fe1bb6f72575d
Varun Mehta <varun@botiga.app>
2020-10-13 01:11:38 +0530

    validation of a textformfield

--- lib/app/login/phoneScreen.dart
+++ lib/app/login/phoneScreen.dart
@@ -5,22 +5,52 @@ import '../../widgets/botigaTextFieldForm.dart';
 
 import 'widgets/background.dart';
 
-class PhoneScreen extends StatelessWidget {
+class PhoneScreen extends StatefulWidget {
   final String title;
 
   PhoneScreen(this.title);
 
   @override
+  _PhoneScreenState createState() => _PhoneScreenState();
+}
+
+class _PhoneScreenState extends State<PhoneScreen> {
+  GlobalKey<FormState> _phoneFormKey;
+  FocusNode _phoneFocusNode;
+
+  void _phoneListener() {
+    if (!_phoneFocusNode.hasFocus) {
+      _phoneFormKey.currentState.validate();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _phoneFormKey = GlobalKey<FormState>();
+    _phoneFocusNode = FocusNode();
+
+    _phoneFocusNode.addListener(_phoneListener);
+  }
+
+  @override
+  void dispose() {
+    // Clean up the focus node when the Form is disposed.
+    _phoneFocusNode.removeListener(_phoneListener);
+    _phoneFocusNode.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Background(
-      title: title,
-      child: Form(
-        child: Column(
-          children: [
-            textField(),
-            formButton(),
-          ],
-        ),
+      title: widget.title,
+      child: Column(
+        children: [
+          textField(),
+          formButton(),
+        ],
       ),
     );
   }
@@ -29,14 +59,24 @@ class PhoneScreen extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 40, bottom: 30),
       child: BotigaTextFieldForm(
+        formKey: _phoneFormKey,
+        focusNode: _phoneFocusNode,
         labelText: 'Phone Number',
         validator: (value) {
           if (value.isEmpty) {
             return 'Required';
+          } else if (value.length != 10) {
+            return 'Phone number should have 10 digits';
           }
           return null;
         },
         keyboardType: TextInputType.number,
+        onChange: (val) {
+          if (val.length == 10) {
+            // hide keyboard as there is no done button on number keyboard
+            FocusScope.of(context).unfocus();
+          }
+        },
       ),
     );
   }
@@ -49,6 +89,7 @@ class PhoneScreen extends StatelessWidget {
           borderRadius: new BorderRadius.circular(8.0),
         ),
         onPressed: () {
+          if (_phoneFormKey.currentState.validate()) {}
           // Navigator.of(context)
           //     .pushNamed(SignUpOtp.routeName);
         },
--- lib/widgets/botigaTextFieldForm.dart
+++ lib/widgets/botigaTextFieldForm.dart
@@ -2,65 +2,100 @@ import 'package:flutter/material.dart';
 
 import '../theme/index.dart';
 
+/*
+ *	Params Explained
+ *	- focusNode - Used to create a focus reference for a node
+ *	- nextFocusNode - Pass the focus node value of the next text field
+ */
+
 class BotigaTextFieldForm extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
   final String labelText;
   final Function(String) validator;
   final TextInputType keyboardType;
   final TextInputAction textInputAction;
   final FocusNode focusNode;
   final int maxLines;
+  final int maxLength;
   final FocusNode nextFocusNode;
+  final Function(String) onFieldSubmitted;
+  final TextEditingController textEditingController;
+  final Function(String) onChange;
 
   BotigaTextFieldForm({
+    @required this.formKey,
+    @required this.focusNode,
     @required this.labelText,
     @required this.validator,
     this.keyboardType = TextInputType.text,
     this.textInputAction = TextInputAction.done,
     this.maxLines = 1,
-    this.focusNode,
+    this.maxLength,
     this.nextFocusNode,
+    this.onFieldSubmitted,
+    this.textEditingController,
+    this.onChange,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      // showCursor: false,
-      validator: validator,
-      keyboardType: maxLines > 1 ? TextInputType.multiline : keyboardType,
-      textInputAction: textInputAction,
-      maxLines: maxLines,
-      onSaved: (val) => '',
-      cursorColor: AppTheme.primaryColor,
-      focusNode: focusNode,
-      onFieldSubmitted: (_) {
-        FocusScope.of(context).requestFocus(nextFocusNode);
-      },
-      decoration: InputDecoration(
-        prefixIcon: Icon(Icons.phone, color: AppTheme.color50),
-        fillColor: AppTheme.backgroundColor,
-        filled: true,
-        labelText: labelText,
-        labelStyle: AppTheme.textStyle.w500.color50.size(15.0).lineHeight(1.3),
-        hintStyle: AppTheme.textStyle.w500
-            // .size(15.0)
-            // .lineHeight(1.3)
-            .colored(Colors.orange),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 1.0,
-            color: AppTheme.color25,
+    return Form(
+      key: formKey,
+      child: TextFormField(
+        // showCursor: false,
+        validator: validator,
+        keyboardType: maxLines > 1 ? TextInputType.multiline : keyboardType,
+        textInputAction: textInputAction,
+        maxLines: maxLines,
+        onSaved: (val) => '',
+        cursorColor: AppTheme.primaryColor,
+        focusNode: focusNode,
+        maxLength: maxLength,
+        controller: textEditingController,
+        onFieldSubmitted: (value) {
+          if (onFieldSubmitted != null) {
+            onFieldSubmitted(value);
+          }
+          if (nextFocusNode != null) {
+            FocusScope.of(context).requestFocus(nextFocusNode);
+          }
+        },
+        onChanged: onChange,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.phone, color: AppTheme.color50),
+          fillColor: AppTheme.backgroundColor,
+          filled: true,
+          labelText: labelText,
+          labelStyle:
+              AppTheme.textStyle.w500.color50.size(15.0).lineHeight(1.3),
+          errorStyle: AppTheme.textStyle.w400.colored(AppTheme.errorColor),
+          hintStyle: AppTheme.textStyle.w500
+              // .size(15.0)
+              // .lineHeight(1.3)
+              .colored(Colors.orange),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 1.0,
+              color: AppTheme.color25,
+            ),
           ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 1.0,
-            color: AppTheme.color25,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 1.0,
+              color: AppTheme.color25,
+            ),
           ),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 1.0,
-            color: AppTheme.errorColor,
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 1.0,
+              color: AppTheme.errorColor,
+            ),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 1.0,
+              color: AppTheme.errorColor,
+            ),
           ),
         ),
       ),
