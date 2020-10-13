6869efb233d318d75efd099fac147760b1f4cd64
Varun Mehta <varun@botiga.app>
2020-10-13 17:22:07 +0530

    resend timer set

--- lib/app/login/verifyOtp.dart
+++ lib/app/login/verifyOtp.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 import '../../theme/index.dart';
@@ -15,33 +16,76 @@ class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
   GlobalKey<FormState> _form = GlobalKey();
   String pinValue = '';
 
+  Timer _timer;
+  int _start;
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    const sizedBox = SizedBox(height: 32);
+
     return Background(
       title: 'Verify OTP',
       backNavigation: true,
       child: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 20, bottom: 10),
-            child: Text(
-              'Please enter OTP sent to your phone number 99999-99999',
-              style: AppTheme.textStyle.w500.color100.size(15).lineHeight(1.3),
+          sizedBox,
+          Text(
+            'Please enter OTP sent to your phone number 99999-99999',
+            style: AppTheme.textStyle.w500.color100.size(15).lineHeight(1.3),
+          ),
+          sizedBox,
+          Form(
+            key: _form,
+            child: PinTextField(
+              pins: 6,
+              onSaved: (val) => pinValue = val,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 32.0),
-            child: Form(
-              key: _form,
-              child: PinTextField(
-                pins: 6,
-                onSaved: (val) => pinValue = val,
-              ),
-            ),
-          ),
+          SizedBox(height: 12),
+          _start > 0
+              ? Text(
+                  'Resend OTP in ${_start}s',
+                  style:
+                      AppTheme.textStyle.w500.color50.size(13).lineHeight(1.5),
+                )
+              : GestureDetector(
+                  onTap: () {
+                    startTimer();
+                  },
+                  child: Text(
+                    'Resend OTP',
+                    style: AppTheme.textStyle.w500
+                        .colored(AppTheme.primaryColor)
+                        .size(13)
+                        .lineHeight(1.5),
+                  ),
+                ),
+          SizedBox(height: 16),
           formButton(),
         ],
       ),
+    );
+  }
+
+  void startTimer() {
+    _start = 30;
+    const oneSec = const Duration(seconds: 1);
+    _timer = new Timer.periodic(
+      oneSec,
+      (Timer timer) =>
+          setState(() => _start < 1 ? _timer.cancel() : _start = _start - 1),
     );
   }
 
