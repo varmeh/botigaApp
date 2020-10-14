2abf2072e6d5869534816c228b6171d572cd5a03
Varun Mehta <varun@botiga.app>
2020-10-14 15:45:17 +0530

    location permission modal box refactored into a widget

--- lib/app/auth/signup/signupOtpScreen.dart
+++ lib/app/auth/signup/signupOtpScreen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../../../theme/index.dart';
 import '../widgets/index.dart';
 import '../model/otpSessionModel.dart';
+import '../../location/locationPermissionModal.dart';
 
 class SignupOtpScreen extends StatelessWidget {
   static final route = 'signupOtp';
@@ -20,83 +20,10 @@ class SignupOtpScreen extends StatelessWidget {
           context: context,
           backgroundColor: Colors.transparent,
           builder: (context) {
-            return locationAccessModal(context);
+            return LocationPermissionModal();
           },
         );
       },
-    );
-  }
-
-  Widget locationAccessModal(BuildContext context) {
-    const sizedBox24 = SizedBox(height: 24);
-    final buttonShape = RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(8.0),
-    );
-
-    return Container(
-      height: 380,
-      width: double.infinity,
-      padding: const EdgeInsets.only(left: 22, right: 22, top: 42),
-      decoration: BoxDecoration(
-        color: AppTheme.backgroundColor,
-        borderRadius: BorderRadius.only(
-          topLeft: const Radius.circular(16.0),
-          topRight: const Radius.circular(16.0),
-        ),
-      ),
-      child: Container(
-        child: Column(
-          children: [
-            Text(
-              'Permission to location',
-              style:
-                  AppTheme.textStyle.w700.color100.size(20.0).lineHeight(1.25),
-            ),
-            SizedBox(height: 32.0),
-            Image.asset('assets/images/location.png'),
-            sizedBox24,
-            Text(
-              'We require this information to check the service availability of your locations',
-              style:
-                  AppTheme.textStyle.w500.color50.size(15.0).lineHeight(1.35),
-              textAlign: TextAlign.center,
-            ),
-            sizedBox24,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                FlatButton(
-                  minWidth: 150,
-                  height: 50,
-                  onPressed: () {},
-                  color: AppTheme.color05,
-                  child: Text(
-                    'Deny',
-                    style: AppTheme.textStyle.w600.color100
-                        .size(15.0)
-                        .lineHeight(1.5),
-                  ),
-                  shape: buttonShape,
-                ),
-                FlatButton(
-                  minWidth: 150,
-                  height: 50,
-                  onPressed: () {},
-                  color: AppTheme.primaryColor,
-                  child: Text(
-                    'Grant Access',
-                    style: AppTheme.textStyle.w600
-                        .size(15.0)
-                        .lineHeight(1.5)
-                        .colored(AppTheme.backgroundColor),
-                  ),
-                  shape: buttonShape,
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
     );
   }
 }
