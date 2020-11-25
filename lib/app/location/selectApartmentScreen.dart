7cc190d6cc200628a5ab71c08e3f0384b502992f
Varun Mehta <varun@botiga.app>
2020-11-25 19:24:03 +0530

    reloading sellers on apartment change

--- lib/app/location/selectApartmentScreen.dart
+++ lib/app/location/selectApartmentScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../util/index.dart' show KeyStore;
 import '../../widgets/index.dart' show Toast;
+import '../../providers/index.dart' show SellersProvider;
 
 import '../tabbar.dart';
 import 'searchApartmentScreen.dart';
@@ -15,18 +17,21 @@ class SelectApartmentScreen extends StatelessWidget {
       KeyStore.shared
           .setApartment(
               apartmentId: apartment.id, apartmentName: apartment.name)
-          .then(
-            (_) => Navigator.pushNamedAndRemoveUntil(
-              context,
-              Tabbar.route,
-              (route) => false,
-            ),
-          )
-          .catchError(
-            (_) => Toast(
-              message: 'Something went wrong. Select again.',
-            ).show(context),
-          );
+          .then((_) {
+        Provider.of<SellersProvider>(context, listen: false).empty();
+        Navigator.pushAndRemoveUntil(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (_, __, ___) => Tabbar(index: 0),
+            transitionDuration: Duration.zero,
+          ),
+          (route) => false,
+        );
+      }).catchError(
+        (_) => Toast(
+          message: 'Something went wrong. Select again.',
+        ).show(context),
+      );
     });
   }
 }
