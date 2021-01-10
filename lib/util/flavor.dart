ff57fbcaf53bd89fde6b8bc33bbd0d0c7751cd25
Varun Mehta <varun@botiga.app>
2021-01-10 18:23:10 +0530

    flavor code update

--- lib/util/flavor.dart
+++ lib/util/flavor.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
 
-enum _Flavor { dev, prod }
+enum _Flavor { dev, prod, qa }
 
 class Flavor {
   static final Flavor _singleton = Flavor._internal();
@@ -21,6 +21,10 @@ class Flavor {
           _flavor = _Flavor.dev;
           break;
 
+        case "app.botiga.botiga.qa":
+          _flavor = _Flavor.qa;
+          break;
+
         default:
           _flavor = _Flavor.prod;
       }
@@ -28,21 +32,45 @@ class Flavor {
   }
 
   bool get isProduction => _flavor == _Flavor.prod;
+  bool get isQa => _flavor == _Flavor.qa;
 
-  Color get bannerColor => isProduction ? Colors.transparent : Colors.brown;
+  Color get bannerColor {
+    if (isProduction) {
+      return Colors.transparent;
+    } else if (isQa) {
+      return Colors.brown;
+    } else {
+      return Colors.red;
+    }
+  }
 
-  String get bannerName => isProduction ? 'PROD' : 'DEV';
+  String get bannerName {
+    if (isProduction) {
+      return 'PROD';
+    } else if (isQa) {
+      return 'QA';
+    } else {
+      return 'DEV';
+    }
+  }
 
-  String get baseUrl =>
-      isProduction ? 'https://prod.botiga.app' : 'https://dev.botiga.app';
+  String get baseUrl {
+    if (isProduction) {
+      return 'https://prod.botiga.app';
+    } else if (isQa) {
+      return 'https://qa.botiga.app';
+    } else {
+      return 'https://dev.botiga.app';
+    }
+  }
 
-  String get paytmMid =>
-      isProduction ? 'BOTIGA19474156290102' : 'OJdkNI97902555723463';
-
-  String get rpayId =>
-      isProduction ? 'rzp_live_U6Hf0upRNgYgtc' : 'rzp_test_eB9RogNMlDSytd';
-
-  String get paytmTransactionUrl => isProduction
-      ? 'https://securegw.paytm.in'
-      : 'https://securegw-stage.paytm.in';
+  String get rpayId {
+    if (isProduction) {
+      return 'rzp_live_U6Hf0upRNgYgtc';
+    } else if (isQa) {
+      return 'rzp_live_U6Hf0upRNgYgtc';
+    } else {
+      return 'rzp_test_eB9RogNMlDSytd';
+    }
+  }
 }
