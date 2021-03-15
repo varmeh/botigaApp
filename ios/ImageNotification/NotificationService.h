19b24f8d0d423ae9fafe5533953337717a0b5de9
Varun Mehta <varun@botiga.app>
2021-03-15 19:09:20 +0530

    rich notification support added

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Varun Mehta on 15/03/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
