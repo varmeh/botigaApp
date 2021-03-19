2de951ddf87075a01d6951833f3502f98f8a7622
Varun Mehta <varun@botiga.app>
2021-03-19 14:05:47 +0530

    remove dot from banner carsouel if 1 image

--- lib/widgets/bannerCarousel.dart
+++ lib/widgets/bannerCarousel.dart
@@ -25,6 +25,8 @@ class _BannerCarosuelState extends State<BannerCarosuel> {
 
   @override
   Widget build(BuildContext context) {
+    bool hasMoreThan1Banner = widget.bannerList.length > 1;
+
     return Container(
       width: double.infinity,
       child: Column(
@@ -57,33 +59,40 @@ class _BannerCarosuelState extends State<BannerCarosuel> {
             }).toList(),
             options: CarouselOptions(
                 height: 160,
-                viewportFraction: 0.85,
+                viewportFraction: hasMoreThan1Banner ? 0.85 : 0.90,
                 initialPage: 0,
+                enableInfiniteScroll: hasMoreThan1Banner,
                 reverse: false,
-                autoPlay: widget.bannerList.length > 1,
+                autoPlay: hasMoreThan1Banner,
                 enlargeCenterPage: true,
                 scrollDirection: Axis.horizontal,
                 onPageChanged: (index, reason) =>
                     setState(() => _current = index)),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ...widget.bannerList.asMap().entries.map((entry) {
-                return Container(
-                  width: 8.0,
-                  height: 8.0,
-                  margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 4.0),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: _current == entry.key
-                        ? AppTheme.color100
-                        : AppTheme.color25,
-                  ),
-                );
-              }),
-            ],
-          ),
+          hasMoreThan1Banner
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ...widget.bannerList.asMap().entries.map((entry) {
+                      return Container(
+                        width: 8.0,
+                        height: 8.0,
+                        margin: EdgeInsets.symmetric(
+                            vertical: 12.0, horizontal: 4.0),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _current == entry.key
+                              ? AppTheme.color100
+                              : AppTheme.color25,
+                        ),
+                      );
+                    }),
+                  ],
+                )
+              : Padding(
+                  padding: const EdgeInsets.only(top: 12.0),
+                  child: Container(),
+                ),
         ],
       ),
     );
