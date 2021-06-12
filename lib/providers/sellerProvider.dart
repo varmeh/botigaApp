a0e5e3f59f170a3562cf8619c2dbe91110396fd9
Varun Mehta <varun@botiga.app>
2021-06-12 14:13:40 +0530

    get recommended products

--- lib/providers/sellerProvider.dart
+++ lib/providers/sellerProvider.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
-import '../models/index.dart' show CategoryModel, CouponModel;
+import '../models/index.dart' show CategoryModel, CouponModel, ProductModel;
 import '../util/index.dart' show Http;
 
 class SellerProvider with ChangeNotifier {
   Map<String, List<CategoryModel>> _sellerProducts = {};
   Map<String, List<String>> _sellerBanners = {};
   Map<String, List<CouponModel>> _sellerCoupons = {};
+  Map<String, List<ProductModel>> _sellerRecommended = {};
 
   List<CategoryModel> products(String sellerId) =>
       _sellerProducts.containsKey(sellerId) ? _sellerProducts[sellerId] : [];
@@ -17,8 +18,15 @@ class SellerProvider with ChangeNotifier {
   List<CouponModel> coupons(String sellerId) =>
       _sellerCoupons.containsKey(sellerId) ? _sellerCoupons[sellerId] : [];
 
+  List<ProductModel> recommendedProducts(String sellerId) =>
+      _sellerRecommended.containsKey(sellerId)
+          ? _sellerRecommended[sellerId]
+          : [];
+
   bool hasBanners(String sellerId) => banners(sellerId).length > 0;
   bool hasCoupons(String sellerId) => coupons(sellerId).length > 0;
+  bool hasRecommendedProducts(String sellerId) =>
+      recommendedProducts(sellerId).length > 0;
 
   Future<void> getProducts(String sellerId) async {
     if (_sellerProducts.containsKey(sellerId)) {
@@ -42,6 +50,29 @@ class SellerProvider with ChangeNotifier {
           )
           .cast<CategoryModel>()
           .toList();
+
+      bool hasRecommendedProducts = json['hasRecommendedProducts'] as bool;
+      int numberOfRecommendedProducts =
+          json['numberOfRecommendedProducts'] as int;
+
+      if (hasRecommendedProducts && numberOfRecommendedProducts > 0) {
+        _sellerRecommended[sellerId] = [];
+
+        for (CategoryModel category in products(sellerId)) {
+          if (category.showCategory) {
+            for (ProductModel product in category.products) {
+              if (product.recommended) {
+                _sellerRecommended[sellerId].add(product);
+                numberOfRecommendedProducts--;
+
+                if (numberOfRecommendedProducts == 0) {
+                  return;
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
