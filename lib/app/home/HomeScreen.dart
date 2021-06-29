4a2845b66db59f5a4d4f8a5b8da010b673084402
Varun Mehta <varun@botiga.app>
2021-06-29 14:47:56 +0530

    changed aspect ratio for child

--- lib/app/home/HomeScreen.dart
+++ lib/app/home/HomeScreen.dart
@@ -272,7 +272,7 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
         ),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 1,
-          childAspectRatio: 1.8,
+          childAspectRatio: 1.65,
           mainAxisSpacing: _crossAxisSpacing,
         ),
         itemCount: sellers.length,
